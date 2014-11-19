629a5667837214534e9a2d8c237803920f584942
rayallasureshkumar <rayakkasureshkumar@gmail.com>
2014-11-19 11:33:09 +0530

    heroku dep

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,10 @@ module Paper
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    
+    #heroku dep
+config.assets.debug = false
+config.assets.initialize_on_precompile = false
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
